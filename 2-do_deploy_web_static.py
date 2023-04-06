f1618cedc3b710cba76900e9aa6ecd5591d30d8e
isaaccleopas <isaaccleopas@gmail.com>
2023-04-06 18:16:31 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fabfile to distribute an archive to a web server."""
 
-from fabric.api import env, put, run, sudo
+from fabric.api import env, put, run, local
 from os.path import exists
 
 
@@ -9,27 +9,25 @@ env.hosts = ['54.152.246.245', '54.144.141.32']
 env.user = 'ubuntu'
 env.key_filename = '/home/vagrant/.ssh/id_rsa'
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
+    """Deploy a compressed archive to the web servers."""
     if not exists(archive_path):
         return False
-
     try:
-        put(archive_path, '/tmp/')
-        filename = archive_path.split('/')[-1]
-        foldername = '/data/web_static/releases/' + filename[:-4]
-        run('sudo mkdir -p {} && sudo tar -xzf /tmp/{} -C {}'
-            .format(foldername, filename, foldername))
-        run('sudo rm /tmp/{}'.format(filename))
-        run('sudo mv {}/web_static/* {}/'.format(foldername, foldername))
-        run('sudo rm -rf {}/web_static'.format(foldername))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(foldername))
-
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file_name, no_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(no_ext, no_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(no_ext))
         print("New version deployed!")
         return True
-
-    except Exception as e:
-        print("Error: {}".format(e))
+    except:
         return False
