a7ff86a5add4088141b42de1f8dc29beecde988e
isaaccleopas <isaaccleopas@gmail.com>
2023-04-06 16:39:42 +0000

    task 1 corrected

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """Fabric script to create a .tgz archive of web_static contents"""
 
-import os
 from fabric.api import local
 from datetime import datetime
+from os.path import isdir
 
 def do_pack():
-    """Creates a compressed archive of web_static"""
-    if not os.path.exists("versions"):
-        os.makedir("versions")
-    timestamp = datetime.utcnow()
-    archive_filename = "web_static_{}.tgz".format(timestamp)
-    archive_command = "tar -czvf {0} {1}".format(
-            os.path.join("versions", archive_filename),
-            os.pathe.join("web_static")
-    )
-    result = local(archive_command)
-
-    if result.failed:
+    """Creates a compressed archive of web_static content"""
+    if not isdir("versions"):
+        local("mkdir versions")
+    date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+    if local("tar -czvf {} web_static".format(file_name)).failed:
         return None
-    return os.path.join("versions", archive_filename)
+    return file_name
