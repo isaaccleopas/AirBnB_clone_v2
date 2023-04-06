c2125f8ac1b9cf1c44342b6279a2b7a8b0e0319a
isaaccleopas <isaaccleopas@gmail.com>
2023-04-06 09:44:50 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "Welcome To My Test Page" /data/web_static/releases/test/index.html
+echo "Welcome To My Test Page" | sudo tee /data/web_static/releases/test/index.html
 sudo rm -f /data/web_static/current && ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 location_block='\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}'
