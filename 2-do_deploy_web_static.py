742aeafe9d8d551fc2688561511dc2b96cee3434
isaaccleopas <isaaccleopas@gmail.com>
2023-04-06 20:44:52 +0000

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,31 +3,34 @@
     ..an archive to your web servers, using the function do_deploy: """
 
 
-from fabric.api import put, run, local, env 
-from datetime import datetime
+from fabric.api import put, run, local, env
 from os.path import exists
 
 env.hosts = ['54.152.246.245', '54.144.141.32']
-env.user = 'ubuntu'
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
-    filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
 
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        put(archive_path, '/tmp/')
+
+        filename = archive_path.split('/')[-1]
+        foldername = '/data/web_static/releases/' + filename[:-4]
+        run('sudo mkdir -p {} && sudo tar -xzf /tmp/{} -C {}'
+            .format(foldername, filename, foldername))
+
+        run('sudo rm /tmp/{}'.format(filename))
+
+        run('sudo mv {}/web_static/* {}/'.format(foldername, foldername))
+        run('sudo rm -rf {}/web_static'.format(foldername))
+        run('sudo rm -rf /data/web_static/current')
+
+        run('sudo ln -s {} /data/web_static/current'.format(foldername))
+
+        print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
+        print("Error: {}".format(e))
         return False
