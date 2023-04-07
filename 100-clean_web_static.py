35479b310fbac61085e896935d8fc3f8935cade7
isaaccleopas <isaaccleopas@gmail.com>
2023-04-07 06:23:01 +0000

    Keep it clean! corrected

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,17 +7,11 @@ from datetime import datetime
 from fabric.api import env, run, local
 import os
 
-
-env.hosts = ['54.152.246.245', '54.144.141.32']
-
-
 def do_clean(number=0):
     """
     This function deletes out-of-date archives.
-
     Args:
         number (int): The number of archives to keep.
-
     Returns:
         Nothing.
     """
@@ -27,18 +21,13 @@ def do_clean(number=0):
         number = int(number)
 
     local('mkdir -p versions')
-
     archives = local('ls -1t versions', capture=True).split('\n')
-    archives_to_delete = archives[number:]
-
-    for archive in archives_to_delete:
+    for archive in archives[number:]:
         local('rm -rf versions/{}'.format(archive))
 
     run("mkdir -p /data/web_static/releases")
     archives = run("ls -1t /data/web_static/releases").split()
-    archives_to_delete = archives[number:]
-
-    for archive in archives_to_delete:
+    for archive in archives[number:]:
         if archive != "test":
             path = "/data/web_static/releases/{}".format(archive)
             run("rm -rf {}".format(path))
