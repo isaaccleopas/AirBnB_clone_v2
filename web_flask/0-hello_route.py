6a9488f0488793397a8e0e5a8e76619f90a3c1e6
isaaccleopas <isaaccleopas@gmail.com>
2023-04-21 16:33:59 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Starts a Flask web app
-   
-   The app listens on 0.0.0.0, port 5000.
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
